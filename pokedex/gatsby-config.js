8d9947b1b7c01db4cdb431157c319c8fbb344e29
Byakuya91 <nganti91@yahoo.com>
2023-10-09 20:56:36 -0400

    I've implemented the styled components and added folders to give structure. I will be working on the fonts and getting those set before I begin any fetching of the Pokemon data from Nintendo.

--- pokedex/gatsby-config.js
+++ pokedex/gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Pokedex`,
+    description: `Gatsby app to showcase a podex.`,
+    author: `@neilganti`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
---
+++ pokedex/src/styles/homepageStyles.ts
@@ -0,0 +1,9 @@
+import { styled } from "styled-components"
+
+export const HomeTitle = styled.h1`
+  font-size: 4rem;
+  font-family: "Preahvihear";
+  text-align: center;
+  margin-bottom: 2rem;
+  color: ${props => props.theme.colors.primary};
+`
