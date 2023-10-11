61d0992278ea0b4f7493b433c97ae9fae8f9fea9
Byakuya91 <nganti91@yahoo.com>
2023-10-11 17:49:45 -0400

    I've managed to integrate fonts into the project and will be moving onto loading the image component and fetching the data from the Nintendo API.

--- pokedex/gatsby-config.js
+++ pokedex/gatsby-config.js
@@ -18,6 +18,27 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
     {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Roboto`,
+            file: `https://fonts.googleapis.com/css2?family=Roboto&display=swap`,
+          },
+          {
+            name: `Preahvihear`,
+            file: `https://fonts.googleapis.com/css2?family=Preahvihear&display=swap`,
+          },
+        ],
+      },
+    },
+
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
