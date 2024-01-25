85b65b625225e50ff103715ecc6972866b217683
Byakuya91 <nganti91@yahoo.com>
2024-01-25 16:27:34 -0500

    I've finished the grid and layout for the cards and getting the cards color coded based on type. Moving onto drop down filter and search bar, alongside getting into individual card profiles.

--- pokedex/src/components/PokemonCard/pokeIndex.tsx
+++ pokedex/src/components/PokemonCard/pokeIndex.tsx
@@ -1,13 +1,13 @@
 import React from "react"
-import { PokemonCardProps } from "./pokeTypes"
+import { PokemonCardProps } from "./types"
 import { Container } from "./styled"
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon, index }) => {
   return (
-    <Container>
+    <Container types={pokemon?.types[0]}>
       <img src={pokemon.image} alt={pokemon.name} />
       <p>{pokemon.name}</p>
-      <p>{index + 1}</p>
+      <p>{index !== undefined ? index + 1 : null}</p>
     </Container>
   )
 }
--- pokedex/src/components/PokemonCard/pokeTypes.ts
+++ pokedex/src/components/PokemonCard/pokeTypes.ts
@@ -4,8 +4,28 @@ export type PokemonCardProps = {
 }
 
 export type AllPokemonType = {
-  types: string[]
+  types: PokemonTypes[]
   image: string
   id: string
   name: string
 }
+
+export type PokemonTypes =
+  | "water"
+  | "fire"
+  | "grass"
+  | "bug"
+  | "normal"
+  | "poison"
+  | "electric"
+  | "ground"
+  | "fairy"
+  | "fighting"
+  | "psychic"
+  | "rock"
+  | "ghost"
+  | "ice"
+  | "dragon"
+  | "dark"
+  | "steel"
+  | "flying"
